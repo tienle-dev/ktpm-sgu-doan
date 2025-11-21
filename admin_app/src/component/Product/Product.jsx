7a40d9a5d006f7bff6be193be9d643a369c308f3
tienle-dev <lmthaha1414@gmail.com>
2025-11-21 22:31:01 +0700

    fix product description UI in admin

--- admin_app/src/component/Product/Product.jsx
+++ admin_app/src/component/Product/Product.jsx
@@ -97,9 +97,22 @@ function Product() {
                                                     <tr key={index}>
                                                         <td className="name">{value._id}</td>
                                                         <td className="name">{value.name_product}</td>
-                                                        <td>{new Intl.NumberFormat('vi-VN',{style: 'decimal',decimal: 'VND'}).format(value.price_product)+ ' VNĐ'}</td>
+                                                        <td>{new Intl.NumberFormat('vi-VN', { style: 'decimal', decimal: 'VND' }).format(value.price_product) + ' VNĐ'}</td>
                                                         <td><img src={value.image} alt="" style={{ width: '70px' }} /></td>
-                                                        <td className="name" style={{ width: '40px' }}>{value.describe}</td>
+                                                        
+                                                        <td 
+                                                            className="name" 
+                                                            style={{ 
+                                                                maxWidth: '200px', // Giới hạn chiều rộng tối đa
+                                                                overflow: 'hidden', // Ẩn phần thừa
+                                                                textOverflow: 'ellipsis', // Thêm dấu ...
+                                                                whiteSpace: 'nowrap' // Giữ trên 1 dòng (đã có ở class no-wrap của table nhưng thêm cho chắc)
+                                                            }}
+                                                            title={value.describe} // Tooltip: Rê chuột vào sẽ thấy hết nội dung
+                                                        >
+                                                            {value.describe}
+                                                        </td>
+
                                                         <td>{value.id_category ? value.id_category.category : ""}</td>
                                                         <td>
                                                             <div className="d-flex">
